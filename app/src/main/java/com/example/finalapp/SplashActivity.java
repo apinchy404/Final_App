eb3cca5045bbec75d66bd35306dbf960af1433bb
Apinchy404 <nipachowdhury81@gmail.com>
2021-01-28 22:46:32 +0600

    Initial commit

---
+++ app/src/main/java/com/example/finalapp/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.finalapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    int splashTimeOut=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashTimeOut);
+    }
+}
